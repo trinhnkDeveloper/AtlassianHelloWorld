2b4a451dd30692ed0cd66deb0b6f6c09c3840dae
trinhnk <trinhnk.imlv@gmail.com>
2018-11-28 16:52:21 +0700

    Add error action, and finish render data into template. change the title of Hello-World and Questionaire into Xin chao && Phong Van

--- src/main/java/jp/co/kodnet/plugins/sample/questionaire/OutputAction.java
+++ src/main/java/jp/co/kodnet/plugins/sample/questionaire/OutputAction.java
@@ -12,17 +12,30 @@ public class OutputAction extends ConfluenceActionSupport{
     
     private String getParameterValue(ActionContext context, String key){
         Object value = context.getParameters().get(key);
-        System.out.println(value);
-        if(value instanceof String){
-            return (String)value;
-        }
+        System.out.println("i was here");
+        System.out.println("this is value: " + value.toString());
+       if(value instanceof String[] && ((String[])value).length != 0){
+           System.out.println("but i can go here");
+           return ((String[])value)[0];
+       }else if(value instanceof String){
+           System.out.println("no i go here");
+           return (String)value;
+       }
         return null;
     }
 
     @Override
     public String execute() throws Exception {
         ActionContext context = ActionContext.getContext();
-        this.name = this.getParameterValue(context, "name");
+        this.name = this.getParameterValue(context, "txtName");
+        System.out.println("this is name: " + this.name);
+        if(this.name == null || "".equals(this.name)){
+            return ERROR;
+        }
         return SUCCESS;
+    }
+    
+    public String getName(){
+        return name;
     }
 }
