cf86d87b3b386efeef773d845da4226d90b5dd91
trinhnk <trinhnk.imlv@gmail.com>
2018-11-30 16:01:01 +0700

    chinh sua CreateSpace

--- src/main/java/jp/co/kodnet/plugins/sample/allspace/CreateSpace.java
+++ src/main/java/jp/co/kodnet/plugins/sample/allspace/CreateSpace.java
@@ -3,10 +3,12 @@ package jp.co.kodnet.plugins.sample.allspace;
 import aQute.bnd.annotation.component.Component;
 import com.atlassian.confluence.core.ConfluenceActionSupport;
 import com.atlassian.confluence.spaces.SpaceManager;
-import com.atlassian.confluence.user.ConfluenceUser;
 import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
 import com.atlassian.spring.container.ContainerManager;
+import com.atlassian.user.User;
+import com.atlassian.user.UserManager;
 import com.opensymphony.xwork.ActionContext;
+import java.util.Iterator;
 
 /**
  *
@@ -17,6 +19,7 @@ import com.opensymphony.xwork.ActionContext;
 public class CreateSpace extends ConfluenceActionSupport {
 
     private SpaceManager spaceManager = (SpaceManager) ContainerManager.getComponent("spaceManager");
+    private UserManager userManager = (UserManager) ContainerManager.getComponent("userManager");
 
     public SpaceManager getSpaceManager() {
         return spaceManager;
@@ -37,15 +40,21 @@ public class CreateSpace extends ConfluenceActionSupport {
         ActionContext context = ActionContext.getContext();
         String spaceName = getParameter(context, "txtSpaceName");
         String spaceKey = getParameter(context, "txtSpaceKey");
-
-        ConfluenceUser user = getAuthenticatedUser();
-        spaceManager.createSpace(spaceKey, spaceName, "new space", user);
+        
+        Iterator<User> users = userManager.getUsers().iterator();
+        User user = null;
+        while (users.hasNext()) {
+            user = users.next();
+        }
+        if (user != null) {
+            spaceManager.createSpace(spaceKey, spaceName, "new Space", user);
+        } else{
+            return ERROR;
+        }
         return SUCCESS;
     }
 
-    @Override
-    public ConfluenceUser getAuthenticatedUser() {
-        return super.getAuthenticatedUser();
+    public UserManager getUserManager() {
+        return userManager;
     }
-    
 }
